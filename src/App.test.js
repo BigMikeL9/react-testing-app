9a59597d1e2fb88c8c58dafc62bcea150f1fceab
MikeL Kamel <mikel1832@yahoo.com>
2022-11-12 18:20:07 -0500

    Running first test with Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+// This file contains testing code.
+/*
+  - 'test()'  function takes in two arguments. 
+  - FIRST argument is a description of the test 
+      (up to us, can be anything. It helps us identify this test in the testing output)
+
+  - SECOND argument is an anonymous function which contains the actual testing code.
+
+*/
+test("renders learn react link", () => {
+  /* 
+      - 'render()' function is provided to us by the 'testing-library/react' library
+
+      - which renders the component it receives as an argument in our simulated/virtual environment created by the 'testing-library/react' library.
+  */
   render(<App />);
+
+  /* 
+      - get a hold of an element rendered by the '<App/>' component using the     TEXT that is rendered inside of it.
+
+      - so here we are looking for an element with text of 'learn react'. '/learn react/i' => the 'i' means ignore caps, so in a case INSENSITIVE way.
+      
+      - '/learn react/i' is a 🌟 REGULAR expression 🌟
+  */
   const linkElement = screen.getByText(/learn react/i);
+
+  /* 
+      - checks if that element in is the document, ie: if it was rendered in the document.
+      
+      - This test will FAIL if the element is NOT found.
+  */
   expect(linkElement).toBeInTheDocument();
 });
+
+// -------------------------------
+// ---- Running Tests ----
+
+/*  
+    1. in the terminal run this script  -->  'npm test'
+
+    2. then type 'a' which will let Jest look for ALL files in our App that end with '.test.js', and then run ALL the test that are defined in there, with the 'test()' function.
+
+*/
+
+// By default, 'npm test' script watches our files and reruns the tests whenever we save changes in any file in our App.
+// -------------------------------
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+// ----------------------------
+// Mikel -> this file sets up our environment for testing with Jest.
+// Dont need to do anything in this file
+// ----------------------------
